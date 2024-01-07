c791a4bf12aa32aae7e799a0dbb3e4ae88eb3526
Steven Young <steven.young114@gmail.com>
2024-01-07 11:13:35 +1100

    extract transition_logic from driver_io

--- blackjack/driver.py
+++ blackjack/driver.py
@@ -4,7 +4,7 @@ import random
 
 from enum import Enum
 from typing import Callable, Union
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 
 from blackjack import rules, cards, io
 
@@ -19,6 +19,7 @@ class GameStage(Enum):
     ASK_SPLIT = 20
     PLAYER_ACTIONS = 30 
     PLAYER_DONE = 40
+    UPDATE_BANK = 41
     COMPLETE = 50
 
 @dataclass
@@ -39,16 +40,14 @@ def make_deck_epoch():
     seed = random.Random(epoch)
     return cards.make_deck_unordered(seed) 
 
-def driver_io(
-    ext_stop_pred : threading.Event,
-    strings : StringProvider,
-    reader : Callable[..., str] = input,
-    writer : Callable[[str], None] = print
-):
+def transition_logic(data : GameState, strings : StringProvider, reader : Callable[..., str], writer : Callable[[str], None]):
     """
-    Sort of like main() or a rules.loop. It's the highest level driver of program logic and it creates the I/O side effects concerning user input and display.
-    """
+    Given a GameStage and related data, returns the updated data according to blackjack logic and user input. Implemented as a state machine/pattern matching.
 
+    Impure, but is wrappable for pure interface
+
+    @arg data A GameState representing a snapshot of the working state
+    """
     def input_require(dtype, prompt, prompt_fail):
         # The reader and writer do not change so typing it repeatedly is annoying
         return io.input_require(dtype, prompt, prompt_fail, reader, writer)
@@ -101,122 +100,131 @@ def driver_io(
             writer(strings.show_insurance_fail())
         return ins_status, side_bet_winnings
 
-    def transition_logic(data : GameState):
-        """
-        Given a GameStage and related data, returns the updated data according to blackjack logic and user input. Implemented as a state machine/pattern matching.
+    match data.stage:
+        case GameStage.ASK_BET:
+            new_bank = ask_bank()
 
-        Impure, but is wrappable for pure interface
+            #data.deck = make_deck_epoch()
+            data.bank = new_bank
+            data.bet = ask_bet(new_bank)
+            data.stage = GameStage.INIT_DEAL
 
-        @arg data A GameState representing a snapshot of the working state
-        """
-        match data.stage:
-            case GameStage.ASK_BET:
-                new_bank = ask_bank()
+        case GameStage.INIT_DEAL:
+            # deal the player and dealer. modifies the deck via side effect in the process.
+            new_player, new_dealer = handle_init_hand(data.deck)
 
-                data.deck = make_deck_epoch()
-                data.bank = new_bank
-                data.bet = ask_bet(new_bank)
-                data.stage = GameStage.INIT_DEAL
+            if rules.is_natural(new_player):
+                winnings = rules.compare(new_player, new_dealer, data.bet, payout_ord=PayoutOrd.THREE_TWO)
 
-            case GameStage.INIT_DEAL:
-                # deal the player and dealer. modifies the deck via side effect in the process.
-                new_player, new_dealer = handle_init_hand(data.deck)
-
-                if rules.is_natural(new_player):
-                    winnings = rules.compare(new_player, new_dealer, data.bet, payout_ord=PayoutOrd.THREE_TWO)
-
-                    data.bank += winnings
-                    data.stage = GameStage.ASK_BET
-
-                # check and handle insurance
-                elif rules.is_insurable(new_dealer) and ask_want_insurance():
-                    ins_success, side_bet_payout = handle_insurance(new_dealer, data.bet)
-
-                    data.player = new_player
-                    data.dealer = new_dealer
-                    data.bank += side_bet_payout
-                    data.stage = GameStage.ASK_BET if ins_success else GameStage.ASK_SPLIT
-
-                # either not insurable or they don't want insurance, so move on
-                else:
-                    data.player = new_player
-                    data.dealer = new_dealer
-                    data.stage = GameStage.ASK_SPLIT
-    
-            case GameStage.ASK_SPLIT:
-                if rules.can_split(data.player) and ask_want_split():
-
-                    data.player = rules.init_split(data.player, data.deck)
-                    data.stage = GameStage.PLAYER_ACTIONS
-
-                else:
-                    data.stage=GameStage.PLAYER_ACTIONS
-
-            case GameStage.PLAYER_ACTIONS: # this is more granular than "for split in splits", being every hit/stay prompt. I think being less granular isn't as true to the state machine model
-
-                #is_split = all(isinstance(elem,list) for elem in data.player) # a more naive but faster way is to check if first element is tuple
-                is_split = isinstance(data.player[0],list)
-
-                def split_completion():
-                    """
-                    If split hand, moves the last hand into the completed hand list. If not split, wraps the hand into Split representation and moves it into the completed hand list. Finally, progresses the state if it was the last hand.
-
-                    @arg force_complete Do we state transition even if it's not the last hand?
-                    """
-                    if data.player_completed == None:
-                        data.player_completed = []
-
-                    if is_split:
-                        data.player_completed.append(data.player.pop())
-
-                    else:
-                        # data.player.pop() doesn't work because that removes a single card
-                        data.player_completed.append(data.player)
-                        data.player = []
-
-                    if len(data.player) == 0:
-                        data.stage = GameStage.PLAYER_DONE
-
-                current_hand : Hand = data.player[len(data.player)-1] if is_split else data.player
-                if ask_hit():
-                    cards.take_card(current_hand, data.deck)
-
-                    if rules.is_bust(current_hand):
-                        writer(strings.show_bust(current_hand))
-                        split_completion()
-                    else:
-                        writer(strings.show_hand_status(current_hand))
-
-                else:
-                    split_completion()
-
-            case GameStage.PLAYER_DONE:
-                rules.dealer_play(data.dealer, data.deck)
-
-                writer(strings.show_hand_status(data.dealer))
-                data.stage = GameStage.COMPLETE
-
-            case GameStage.COMPLETE:
-                new_bank = sum([rules.compare(hand, data.dealer, data.bet) for hand in data.player_completed])
-                writer(strings.show_bank(new_bank))
-
-                data.bank=data.bank + new_bank
-                data.player_completed = []
+                data.bank += winnings
                 data.stage = GameStage.ASK_BET
 
-            case _:
-                raise StupidProgrammerException(f"missed case {data.stage} in driver.transition_logic")
+            # check and handle insurance
+            elif rules.is_insurable(new_dealer) and ask_want_insurance():
+                ins_success, side_bet_payout = handle_insurance(new_dealer, data.bet)
+
+                data.player = new_player
+                data.dealer = new_dealer
+                data.bank += side_bet_payout
+                data.stage = GameStage.ASK_BET if ins_success else GameStage.ASK_SPLIT
+
+            # either not insurable or they don't want insurance, so move on
+            else:
+                data.player = new_player
+                data.dealer = new_dealer
+                data.stage = GameStage.ASK_SPLIT
+
+        case GameStage.ASK_SPLIT:
+            if rules.can_split(data.player) and ask_want_split():
+
+                data.player = rules.init_split(data.player, data.deck)
+                data.stage = GameStage.PLAYER_ACTIONS
+
+            else:
+                data.stage=GameStage.PLAYER_ACTIONS
+
+        case GameStage.PLAYER_ACTIONS: # this is more granular than "for split in splits", being every hit/stay prompt. I think being less granular isn't as true to the state machine model
+
+            #is_split = all(isinstance(elem,list) for elem in data.player) # a more naive but faster way is to check if first element is a list (cards are tuples)
+            is_split = isinstance(data.player[0],list)
+            current_hand = data.player[len(data.player) - 1] if is_split else data.player
+
+            hand_completed = False
+
+            if ask_hit():
+                cards.take_card(current_hand, data.deck)
+
+                if rules.is_bust(current_hand):
+                    writer(strings.show_bust(current_hand))
+                    hand_completed = True
+                else:
+                    writer(strings.show_hand_status(current_hand))
+
+            else:
+                hand_completed = True
+
+            if hand_completed:
+                # Because splits and hands aren't equal representations we must handle inserting them differently into the player_completed list.
+
+                # Up to this point player_completed has potentially not been used.
+                if data.player_completed == None:
+                    data.player_completed = []
+
+                if is_split:
+                    data.player_completed.append(data.player.pop())
+
+                else:
+                    # data.player.pop() doesn't work because that removes a single card
+                    data.player_completed.append(data.player)
+                    data.player = []
+
+                if len(data.player) == 0:
+                    data.stage = GameStage.PLAYER_DONE
 
 
+        case GameStage.PLAYER_DONE:
+            rules.dealer_play(data.dealer, data.deck)
+
+            writer(strings.show_hand_status(data.dealer))
+            data.stage = GameStage.COMPLETE
+
+        case GameStage.UPDATE_BANK:
+
+            data.bank += sum([rules.compare(hand, data.dealer, data.bet) for hand in data.player_completed])
+            writer(strings.show_bank(data.bank))
+
+            data.player_completed = []
+            data.stage = GameStage.COMPLETE
+
+        case GameStage.COMPLETE:
+
+            for field in fields(data):
+                if field.name not in ['bank', 'deck']:
+                    setattr(data, field, None)
+
+            data.stage = GameStage.ASK_BET
+            #data.bank = None
+            # deck is allowed to persist
+
+        case _:
+            raise StupidProgrammerException(f"missed case {data.stage} in driver.transition_logic")
+
+def driver_io(
+    ext_stop_pred : threading.Event,
+    strings : StringProvider,
+    reader : Callable[..., str] = input,
+    writer : Callable[[str], None] = print
+):
+    """
+    Sort of like main() or a rules.loop. It's the highest level driver of program logic and it creates the I/O side effects concerning user input and display.
+    """
     try:
-        init_state = GameState(GameStage.ASK_BET, None, None, None, None, None, None)
-        #init_state = GameState(GameStage.ASK_BET, [], -1, -1, [], [], [[]])
-        #current_state = copy.deepcopy(init_state)
+        init_state = GameState(GameStage.ASK_BET, make_deck_epoch(), None, None, None, None, None)
         while not ext_stop_pred.is_set():
             #if len(current_state.deck) < 26:
             #    current_state._replace(deck=make_deck_epoch())
             writer(str(init_state.stage))
-            transition_logic(init_state)
+            transition_logic(init_state, strings, reader, writer)
 
     except KeyboardInterrupt:
         writer(strings.show_keyboard_interrupt())
